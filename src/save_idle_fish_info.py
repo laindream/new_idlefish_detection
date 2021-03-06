64ee1151f941f3c97b175475862553297c7ac3b0
wwx <wwxzju@qq.com>
2021-03-06 19:16:41 +0800

    modify the response item title from idlefish.

--- src/save_idle_fish_info.py
+++ src/save_idle_fish_info.py
@@ -7,13 +7,18 @@ from mitmproxy import ctx, http
 
 import urllib.parse as urlparse
 
+import logging
+
 import json
 import mitmproxy
 import pymongo
 import time
 import re
 
-title_filter_exp = '果|iphone'
+title_filter_exp = '果|iphone|国行|美版'
+
+title_filter_bad_exp = '求购|是收|收收收|回收|本店|装逼神器|收一台|✅'
+
 import_keyword_exp = '在保|保修'
 
 mongoClient = pymongo.MongoClient('mongodb://localhost:27017')
@@ -25,11 +30,13 @@ block_nickname = mydb['block_nickname']
 q_res = block_nickname.find({}, {"_id":0,'nickname': 1})
 block_nickname_set = set([i['nickname'] for i in q_res])
 
+print("block_nickname_set = " + str(len(block_nickname_set)))
 
 
 
 
 def response(flow: mitmproxy.http.HTTPFlow):
+    
     # 忽略非 taobao 搜索地址
     if flow.request.host != "acs.m.taobao.com":
         return
@@ -45,30 +52,21 @@ def response(flow: mitmproxy.http.HTTPFlow):
         data = result['data']
         result_list = data["resultList"]
         for i in result_list:
-            
+            is_bad_item = False
             item = {}
             clickParam = i['data']['item']['main']["clickParam"]
             args = clickParam['args']
             exContent = i['data']['item']['main']["exContent"]
+            richTitle = exContent.get('richTitle')
             detailParams = exContent.get('detailParams')
             if(args.get('item_type') == 'goods' and detailParams != None):
                 count = count + 1
-                print(str(count) + ". " + i['data']['item']['main']["exContent"]['title'])
                 item['title']   = detailParams['title']
-                res = re.findall(title_filter_exp, item['title'].lower())
-                if(len(res) <1): #标题不合
-                    print('filter title:' + item['title'])
-                    continue
-
-
                 item['price']   = int(exContent['price'][1]['text'])
                 item['nickname']    = detailParams['userNick']
-                if(item['nickname'] in block_nickname_set): # nickname被屏蔽
-                    print('block nickname: ' + item['nickname'])
-                    continue
-
                 item['tag']     = [urlparse.unquote(args['q'])]
                 item['item_id'] = int(args['item_id'])
+                item['want'] = 0
                 if(args.get('wantNum') != None):
                     item['want']    = int(args.get('wantNum'))
                 item['area']    = exContent['area']
@@ -76,18 +74,53 @@ def response(flow: mitmproxy.http.HTTPFlow):
                 item['time_str'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
                 item['desc']    = item['title']
                 item['pic_url']     = exContent['picUrl']
+
+                str_out = ""
+
+                
+                # 过滤标题
+                res = re.findall(title_filter_exp, item['title'].lower())
+                res_bad = re.findall(title_filter_bad_exp, item['title'].lower().replace('\n', ' '))
+                if(len(res) <1 or len(res_bad) > 0): #标题不合
+                    is_bad_item = True
+                    str_out += 'filter title, '
+                # 过滤nickname
+                if(item['nickname'] in block_nickname_set): # nickname被屏蔽
+                    is_bad_item = True
+                    str_out += ' block nickname, '
+                # 过滤已存在
                 item_exists = db_items.find_one({'item_id': item['item_id']})
                 if(item_exists):
+                    is_bad_item = True
+                    str_out = '### item already exists! ###'
+                    del item['time']
+                    del item['time_str']
+                    del item['desc']
                     myquery = { "item_id": item['item_id'] }
                     newvalues = { "$set": item}
                     res = db_items.update_one(myquery, newvalues)
-                    print("item already exists!")
-                    print( "modified_count: " + str(res.modified_count))
+
+                # 
+                if(is_bad_item):
+                    exContent['title'] = detailParams['title'] = "++++++++++++++"
+                    if(len(richTitle) > 1):
+                        richTitle[1]['data']['text'] = "+++++++++++++"
+                    #print(str_out)
+                    continue
                 else:
-                    res = db_items.insert_one(item)
-                    print( "inserted_id: " + str(res.inserted_id))
+                    print("%d. [￥%s][%d][%s][%s]"%(count, item['price'], item['want'], item['nickname'], item['title']) )
+
+
+                res = db_items.insert_one(item)
+                print( "inserted_id: " + str(res.inserted_id))
         print('end')
         #print(flow.response.get_text())
+        flow.response.set_text(json.dumps(result))
+
+        with open('resonsen_modified.json','w') as f:
+            f.write(json.dumps(result))
+            f.close()
+
 
     # 将响应中所有“搜索”替换为“请使用谷歌”
     # text = flow.response.get_text()
